8a2e120a5651b67e1bad47d6d1de73bdf8f465d7
Heanzy Zabala <heanzy.zabala@gmail.com>
2022-06-10 09:04:19 +0800

    Updated swagger

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,5 +2,5 @@ import { Router } from 'express'
 import * as files from './files'
 
 export const setupRoutes = (router: Router) => {
-  router.post('/files/extract', files.post)
+  router.post('/files/convert', files.post)
 }
