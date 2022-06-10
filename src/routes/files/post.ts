e5910ed0cd8ed155caff8b740234689861e14fef
Heanzy Zabala <heanzy.zabala@gmail.com>
2022-06-10 09:44:12 +0800

    Added request id and logger

--- src/routes/files/post.ts
+++ src/routes/files/post.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from 'express'
+import { v4 as uuid } from 'uuid'
 
 import * as fileService from '../../services'
 import { logger } from '../../shared'
@@ -8,13 +9,19 @@ export const post = async (
   res: Response,
   next: NextFunction,
 ): Promise<void> => {
+  const requestId = req.headers['Request-Id'] || uuid()
   try {
+    logger.info({ requestId }, 'Processing file')
+
     const csv = await fileService.download(req.body.source)
     const json = fileService.toJSON(csv)
     const zip = fileService.zip(json)
     const url = await fileService.upload(`${req.body.source}.zip`, zip)
+
+    logger.info({ requestId }, 'Finished processing file')
     res.status(200).json({ result: url, rows: json.length })
   } catch (err) {
+    logger.error({ requestId, err }, 'Failed to process file')
     next(err)
   }
 }
